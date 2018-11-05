aa66c0f35f774596e4072f5892d673253e0f2c82
a.tkach <tkach.aleksandr.s@gmail.com>
2018-11-05 13:55:26 +0200

    fix(vuex) import store

--- src/Commands/Presets/vuex/stubs/vue/store/index.js
+++ src/Commands/Presets/vuex/stubs/vue/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import auth from './modules/auth/index';
+import auth from './modules/auth';
 
 Vue.use(Vuex);
 
--- src/Commands/Presets/vuex/stubs/vue/store/modules/auth/mutations.js
+++ src/Commands/Presets/vuex/stubs/vue/store/modules/auth/mutations.js
@@ -1,5 +1,7 @@
 import * as types from './mutation-types';
 
 export default {
-
+    [types.EXAMPLE](state, payload) {
+        state.example = payload;
+    },
 };
