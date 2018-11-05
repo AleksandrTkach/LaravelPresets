a64d54a963a99c2adbe62ea10f9f1b48db445d7d
a.tkach <tkach.aleksandr.s@gmail.com>
2018-11-05 17:56:38 +0200

    fix(spa)

--- src/Commands/Presets/spa/Spa.php
+++ src/Commands/Presets/spa/Spa.php
@@ -9,7 +9,6 @@ class Spa extends Preset
     public static function install()
     {
         static::createSpaController();
-        static::createLayout();
     }
 
     protected static function createSpaController()
@@ -17,54 +16,13 @@ class Spa extends Preset
         if (!file_exists(base_path('app/Http/Controllers/SpaController.php'))) {
             file_put_contents(
                 base_path('app/Http/Controllers/SpaController.php'),
-                file_get_contents(__DIR__ . '/stubs/SpaController.php')
+                file_get_contents(__DIR__ . '/stubs/Controllers/SpaController.php')
             );
         }
 
         file_put_contents(
             base_path('routes/web.php'),
-            file_get_contents(__DIR__ . '/stubs/web.php')
+            file_get_contents(__DIR__ . '/stubs/routes/web.php')
         );
-    }
-
-    protected static function createLayout()
-    {
-        if (!is_dir(base_path('resources/views/layouts'))) {
-            mkdir(base_path('resources/views/layouts'), 0755, true);
-        }
-
-        $file_put_contents = [
-            [
-                'from' => '/stubs/app.blade.php',
-                'to' => 'resources/views/layouts/app.blade.php',
-            ],[
-                'from' => '/stubs/home.blade.php',
-                'to' => 'resources/views/home.blade.php',
-            ],[
-                'from' => '/stubs/app.js',
-                'to' => 'resources/js/app.js',
-            ],[
-                'from' => '/stubs/App.vue',
-                'to' => 'resources/js/components/App.vue',
-            ],
-        ];
-
-        foreach ($file_put_contents as $path) {
-            file_put_contents(
-                base_path($path['to']),
-                file_get_contents(__DIR__ . $path['from'])
-            );
-        }
-
-        $unlinks = [
-            'resources/views/welcom.blade.php',
-            'resources/js/ExampleComponent.vue',
-        ];
-
-        foreach ($unlinks as $path) {
-            if (file_exists(base_path($path))) {
-                unlink($path);
-            }
-        }
     }
 }
